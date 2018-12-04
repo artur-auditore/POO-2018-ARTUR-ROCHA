3a7af3c55dd091b29fbdbd6a0c9cb209be6f4bf1
unknown <defteros7@gmai.com>
2018-12-04 20:04:48 -0300

    simples saudação na tela 2

--- AppNutrientes/app/src/main/java/com/example/artur/appnutrientes/MainActivity.kt
+++ AppNutrientes/app/src/main/java/com/example/artur/appnutrientes/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.example.artur.appnutrientes
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
+
+//TMB = (11,3 x peso (kg)) + (16 x altura (metros)) + 901
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var editNome: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpViews()
+    }
+
+    private fun setUpViews() {
+        editNome = edit_nome
+    }
+
+    fun prosseguir(view: View){
+
+        val nome = editNome.text.toString()
+        val intent = Intent(this, UserInfoActivity::class.java)
+
+        intent.putExtra("nome", nome)
+        startActivity(intent)
     }
 }
+
+
--- AppNutrientes/app/src/main/java/com/example/artur/appnutrientes/UserInfoActivity.kt
+++ AppNutrientes/app/src/main/java/com/example/artur/appnutrientes/UserInfoActivity.kt
@@ -2,11 +2,25 @@ package com.example.artur.appnutrientes
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_user_info.*
 
 class UserInfoActivity : AppCompatActivity() {
+
+    private lateinit var textSaudacao: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_info)
+
+        setUpViews()
     }
+
+    private fun setUpViews() {
+        textSaudacao = text_saudacao
+
+        val saudacao = "Olá ${intent.getStringExtra("nome")}, digite suas informações"
+        textSaudacao.text = saudacao
+    }
+
 }
