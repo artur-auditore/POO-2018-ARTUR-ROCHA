7e4bc9987eaef4c407a71b97c07948a226cc4982
unknown <defteros7@gmai.com>
2018-12-06 00:08:52 -0300

    Radio group e estilos de vida ok

--- AppNutrientes/app/src/main/java/com/example/artur/appnutrientes/MainActivity.kt
+++ AppNutrientes/app/src/main/java/com/example/artur/appnutrientes/MainActivity.kt
@@ -7,12 +7,11 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
-import android.widget.RadioGroup
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.DecimalFormat
+import android.widget.RadioButton
+import kotlinx.android.synthetic.main.activity_user_info.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,10 +19,17 @@ class MainActivity : AppCompatActivity() {
     object constants{
         const val NOME = "nome"
         const val REQUEST_CODE = 1
+        const val SEDENTARIO = 1.0
+        const val LV_ATIVO = 1.15
+        const val MD_ATIVO = 1.25
+        const val MUITO_ATIVO = 1.48
     }
     private lateinit var editNome: EditText
     private lateinit var textTBM: TextView
     private lateinit var textoSelecione: TextView
+    private var tbmAtual: Double = 0.0
+    private lateinit var buttonDetalhes: Button
+
     private lateinit var botoes: RadioGroup
     var decimalFormat = DecimalFormat("#####.#")
 
@@ -40,6 +46,7 @@ class MainActivity : AppCompatActivity() {
 
         textoSelecione = text_selecione
         botoes = botoes_radio_group
+        buttonDetalhes = button_detalhes
     }
 
     fun prosseguir(view: View){
@@ -63,6 +70,7 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == constants.REQUEST_CODE){
             if (resultCode == Activity.RESULT_OK){
                 val tbm = data!!.getDoubleExtra(UserInfoActivity.constants.TBM, 0.0)
+                tbmAtual = tbm
                 textTBM.text = "${decimalFormat.format(tbm)} kcal"
 
                 textTBM.visibility = View.VISIBLE
@@ -73,8 +81,48 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @SuppressLint("SetTextI18n")
+    fun btnSelecionado(view: View){
+        val checked = (view as RadioButton).isChecked
+        val result: Double
 
+        when(view.id){
 
+            R.id.sedentario ->{
+                if (checked) {
+                    result = tbmAtual * constants.SEDENTARIO
+                    textTBM.text = "${decimalFormat.format(result)} kcal"
+                }
+            }
+
+            R.id.levemente_ativo ->{
+                if (checked) {
+                    result = tbmAtual * constants.LV_ATIVO
+                    textTBM.text = "${decimalFormat.format(result)} kcal"
+                }
+            }
+
+            R.id.moderadamente_ativo ->{
+                if (checked) {
+                    result = tbmAtual * constants.MD_ATIVO
+                    textTBM.text = "${decimalFormat.format(result)} kcal"
+                }
+            }
+
+            R.id.muito_ativo ->{
+                if (checked) {
+                    result = tbmAtual * constants.MUITO_ATIVO
+                    textTBM.text = "${decimalFormat.format(result)} kcal"
+                }
+            }
+        }
+        buttonDetalhes.visibility = View.VISIBLE
+    }
+
+    fun detalhes(view: View){
+        //TODO por enquanto
+        startActivity(Intent(this, DetalhesActivity::class.java))
+    }
 
 }
 
