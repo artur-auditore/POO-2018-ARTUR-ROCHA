08c4966e9ad4039141a4b0b4026b3ed8e01aed48
unknown <defteros7@gmai.com>
2018-12-05 23:25:15 -0300

    TBM e troca de dados ok

--- AppNutrientes/app/src/main/java/com/example/artur/appnutrientes/MainActivity.kt
+++ AppNutrientes/app/src/main/java/com/example/artur/appnutrientes/MainActivity.kt
@@ -1,17 +1,32 @@
 package com.example.artur.appnutrientes
 
+
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.DecimalFormat
 
-//TMB = (11,3 x peso (kg)) + (16 x altura (metros)) + 901
 
 class MainActivity : AppCompatActivity() {
 
+    object constants{
+        const val NOME = "nome"
+        const val REQUEST_CODE = 1
+    }
     private lateinit var editNome: EditText
+    private lateinit var textTBM: TextView
+    private lateinit var textoSelecione: TextView
+    private lateinit var botoes: RadioGroup
+    var decimalFormat = DecimalFormat("#####.#")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,16 +36,46 @@ class MainActivity : AppCompatActivity() {
 
     private fun setUpViews() {
         editNome = edit_nome
+        textTBM = text_tbm
+
+        textoSelecione = text_selecione
+        botoes = botoes_radio_group
     }
 
     fun prosseguir(view: View){
 
         val nome = editNome.text.toString()
-        val intent = Intent(this, UserInfoActivity::class.java)
+        if (nome.trim() == ""){
+            Toast.makeText(this, "Nome Inválido!", Toast.LENGTH_SHORT).show()
+        } else{
 
-        intent.putExtra("nome", nome)
-        startActivity(intent)
+            val intent = Intent(this, UserInfoActivity::class.java)
+
+            intent.putExtra(constants.NOME, nome)
+            startActivityForResult(intent, constants.REQUEST_CODE)
+        }
     }
+
+    @SuppressLint("SetTextI18n")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == constants.REQUEST_CODE){
+            if (resultCode == Activity.RESULT_OK){
+                val tbm = data!!.getDoubleExtra(UserInfoActivity.constants.TBM, 0.0)
+                textTBM.text = "${decimalFormat.format(tbm)} kcal"
+
+                textTBM.visibility = View.VISIBLE
+                textoSelecione.visibility = View.VISIBLE
+                botoes.visibility = View.VISIBLE
+
+            }
+        }
+    }
+
+
+
+
 }
 
 
--- AppNutrientes/app/src/main/java/com/example/artur/appnutrientes/UserInfoActivity.kt
+++ AppNutrientes/app/src/main/java/com/example/artur/appnutrientes/UserInfoActivity.kt
@@ -1,26 +1,66 @@
 package com.example.artur.appnutrientes
 
+
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_user_info.*
 
 class UserInfoActivity : AppCompatActivity() {
 
+    object constants{
+        const val TBM = "tbm"
+    }
     private lateinit var textSaudacao: TextView
+    private lateinit var editPeso: EditText
+    private lateinit var editAltura: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_info)
 
         setUpViews()
+        saudacao()
     }
 
     private fun setUpViews() {
         textSaudacao = text_saudacao
 
+        editPeso = edit_peso
+        editAltura = edit_altura
+    }
+
+    private fun saudacao(){
         val saudacao = "Olá ${intent.getStringExtra("nome")}, digite suas informações"
         textSaudacao.text = saudacao
     }
 
+    //TMB = (11,3 x peso (kg)) + (16 x altura (metros)) + 901
+    fun calculoTBM(peso: Double, altura: Double): Double{
+        return ((11.3 * peso) + (16 * altura) + 901)
+    }
+
+    fun calcularTBM(view: View){
+
+        val peso = editPeso.text.toString()
+        val altura = editPeso.text.toString()
+
+        if (peso.trim() == "" || altura.trim() == ""){
+            Toast.makeText(this, "Digite valores válidos!", Toast.LENGTH_SHORT).show()
+        } else {
+
+            val tbm = calculoTBM(peso.toDouble(), altura.toDouble())
+
+            val intent = Intent()
+            intent.putExtra(constants.TBM, tbm)
+
+            setResult(Activity.RESULT_OK, intent); finish()
+        }
+
+    }
 }
